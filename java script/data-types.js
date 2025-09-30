50fc6be2ab7b9f33b190daf31976d97171e690c3
ankur-upflairs <ankur.upflairs@gmail.com>
2025-09-30 21:42:05 +0530

    code pushed

---
+++ java script/data-types.js
@@ -0,0 +1,22 @@
+let b=56
+console.log(b)
+//variables=> it is the name of memory location where values are stored 
+//let/var / const
+// naming rules => 
+/*    1.it can contain digits , alphabets ,_ , $ 
+    2. name can't start with a digit
+    3. we can't use reserve keywords 
+    if = > condition ,for => loops 
+*/ 
+let c1 //declare a variable
+c1=45
+// let 1c X
+// Number => integer + float 
+let c=45.45
+console.log(typeof c)
+//string => normal text (collection of characters)
+
+let d= 'hello'
+let e = "everyone"
+console.log(typeof d , typeof e)
+
