337a3de69e687bec6d868318f5e2f81ef88a5958
ankur-upflairs <ankur.upflairs@gmail.com>
2025-10-06 21:32:29 +0530

    code pushed

---
+++ java script/array.js
@@ -0,0 +1,53 @@
+//array => collection of data types 
+let a = [34,'test' , true ]
+//indexing  is a way to access elements inside array ,indexing starts with 0
+
+console.log(a[1])
+console.log(a.at(2))
+// console.log(a.length)
+// a.pop()
+// a.push(45)
+// a.shift()
+// a.unshift(23)
+let b = [2,3,5,7,19,5]
+// a.includes(5)
+// b.indexOf(5,3)
+// console.log(b.lastIndexOf(5))
+// b.reverse()
+// console.log(b)
+// console.log(b.slice(2,5))
+// console.log(b.sort((a,b)=>b-a))
+// b.splice(3,0,34,23,12)
+// console.log(b)
+// console.log(b.concat([89,90])) 
+
+// for(let i =0 ; i < b.length ; i++){
+//     console.log(b[i])
+// }
+// for(let i in b){
+//     // console.log(b[i])
+// }
+// for(let element of b ){
+//     console.log(element)
+// }
+//all callback methods
+// => 3 arguments => value/element , index  , complete array
+// b.forEach((v,i,arr)=>{
+//     console.log(v,i,arr)
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ java script/dom/dom2.js
@@ -0,0 +1,28 @@
+let ul = document.getElementById('ul')
+// ul.firstElementChild.style.color='red'
+// ul.lastElementChild.style.color='blue' 
+
+// console.log(ul.firstElementChild)
+let li1 = document.getElementById('li1')
+// li1.parentElement.style.margin='50px'
+// console.log(li1.parentElement)
+// li1.nextElementSibling.nextElementSibling.style.color = 'yellow'
+let li5 = document.getElementById('li5')
+// li5.previousElementSibling.innerText ='djkfdjkfj' 
+
+let li6 = document.createElement('li')
+li6.innerText = 'this is new list item'
+// li6.setAttribute('id','li6')
+li6.id='li6'
+// console.log(li6.__proto__) 
+// ul.append(li6)
+// ul.prepend(li6)
+// ul.before(li6)
+// ul.after(li6)
+// ul.insertAdjacentElement('beforeend',li6)
+// li5.remove()
+
+
+
+
+
